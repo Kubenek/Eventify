9e761724401eaf9029a4e67c64fbb16a7157f03f
LisZLisowni2 <rafalkozikowsk735@gmail.com>
2025-03-25 15:24:39 +0100

    Add comments and mongodb conection

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,35 @@
+// Import environment variables
 require('dotenv').config()
 
+// Import packages
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+// Create an app
 const app = express()
 
+// Config variable for all backend app
 const config = {
     PORT: process.env.PORT || 3000,
     MONGODB_URI: process.env.MONGODB_URI
 }
 
+// Connect to MongoDB database
+mongoose.connect(config.MONGODB_URI)
+const db = mongoose.connection
+db.on('error', (err) => console.error(`Error: ${err}`))
+db.on('connect', () => console.error(`Connected to MongoDB`))
+db.on('disconnect', () => console.error(`Disconnected from MongoDB`))
+
+// Attach JSON parser and CORS handler to application
 app.use(express.json())
 app.use(cors({
     origin: ["*"], // temporary
     credentials: true
 }))
 
+// Run server
 app.listen(config.PORT, () => {
     console.log(`Server running at http://localhost:${config.PORT}`)
 })
