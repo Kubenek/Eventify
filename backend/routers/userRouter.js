fad0dc5d364d3f0d022726bfa01eb0dbfa04c5fe
LisZLisowni2 <rafalkozikowsk735@gmail.com>
2025-03-25 16:01:29 +0100

    Add register path

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+const router = express.router()
+
+module.exports = (config = null) => {
+    router.post('/register', async (req, res) => {
+        try {
+            const { username, email, password } = req.body
+            if (!username || !email || !password) return res.status(404).json({ 'message': 'Username, email or password not present' })
+            const hashedPass = bcrypt.hashSync(password, 10)
+            const newUser = new User({ username: username, email: email, password: hashedPass })
+            await newUser.save()
+            res.status(201).json({ 'message': 'Account has been created succesfully' })
+        } catch (err) {
+            res.status(500).json({ 'message': 'Internal server error' })
+        }
+    })
+    
+    return router
+}
